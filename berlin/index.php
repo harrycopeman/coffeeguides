c5ccea47c4a3026d227fb49bf36f971b6ad7f0d8
Harry Copeman <harry.copeman@gmail.com>
2015-03-25 15:11:01 +0000

    Berlin Cafe Selection

--- berlin/index.php
+++ berlin/index.php
@@ -22,10 +22,10 @@ include ("../_include/light-navigation.php");
 		<div class="loop-12">
 			<div class="loop-12">
 				<p class="item-name">Five Elephant</p>
-				<p class="item-desc">Established in 2009 by owner James Dickson, Workshop Coffee specialises in the sourcing and roasting of speciality coffee. Supported by an experienced coffee-roasting department, Workshop offers fresh and enthusiastically researched brews in.</p>
+				<p class="item-desc">Besides making the best cheese cake in Berlin, Five Elephant is famous for excellent roasting and supplies many of Berlin’s prettiest cafés with coffee.</p>
 				<ul>
-					<a href="http://www.workshopcoffee.com/"><li><span class="link-icon">workshopcoffee.com</span></li></a>
-					<li><span class="location-icon">27 Clerkenwell Road</span></li>
+					<a href="http://www.fiveelephant.com/"><li><span class="link-icon">fiveelephant.com</span></li></a>
+					<li><span class="location-icon">Reichenberger Straße 101</span></li>
 				</ul>
 				<div class="wide-image">
 					<img src="/_core/imgs/london/workshop.png"/>
@@ -40,10 +40,10 @@ include ("../_include/light-navigation.php");
 		<div class="loop-12">
 			<div class="loop-12">
 				<p class="item-name">No Fire No Glory</p>
-				<p class="item-desc">Established in 2009 by owner James Dickson, Workshop Coffee specialises in the sourcing and roasting of speciality coffee. Supported by an experienced coffee-roasting department, Workshop offers fresh and enthusiastically researched brews in.</p>
+				<p class="item-desc">No Fire No Glory is also located in Prenzlauer Berg’s Kaffeeviertel. They not only offer very good coffee drinks in a laid-back atmosphere, but also super-yummy sweet treats made by Laura of Tausendsuend.</p>
 				<ul>
-					<a href="http://www.workshopcoffee.com/"><li><span class="link-icon">workshopcoffee.com</span></li></a>
-					<li><span class="location-icon">27 Clerkenwell Road</span></li>
+					<a href="http://www.nofirenoglory.de/"><li><span class="link-icon">nofirenoglory.de</span></li></a>
+					<li><span class="location-icon">Rykestraße 45</span></li>
 				</ul>
 				<div class="wide-image">
 					<img src="/_core/imgs/london/workshop.png"/>
@@ -58,10 +58,10 @@ include ("../_include/light-navigation.php");
 		<div class="loop-12">
 			<div class="loop-12">
 				<p class="item-name">Chapter One</p>
-				<p class="item-desc">Established in 2009 by owner James Dickson, Workshop Coffee specialises in the sourcing and roasting of speciality coffee. Supported by an experienced coffee-roasting department, Workshop offers fresh and enthusiastically researched brews in.</p>
+				<p class="item-desc">Under Czech and German ownership, this cafe’s specialty is slow coffee, offering a Siphon Brew Bar with a huge beans menu for you to choose from. But don’t fret, the staff is always available to guide you through the coffee specialities to find the brew of your liking.</p>
 				<ul>
-					<a href="http://www.workshopcoffee.com/"><li><span class="link-icon">workshopcoffee.com</span></li></a>
-					<li><span class="location-icon">27 Clerkenwell Road</span></li>
+					<a href="http://www.chapter-one-coffee.com/"><li><span class="link-icon">chapter-one-coffee.com</span></li></a>
+					<li><span class="location-icon">Mittenwalder Straße 30</span></li>
 				</ul>
 				<div class="wide-image">
 					<img src="/_core/imgs/london/workshop.png"/>
@@ -76,10 +76,10 @@ include ("../_include/light-navigation.php");
 		<div class="loop-12">
 			<div class="loop-12">
 				<p class="item-name">The Barn</p>
-				<p class="item-desc">Established in 2009 by owner James Dickson, Workshop Coffee specialises in the sourcing and roasting of speciality coffee. Supported by an experienced coffee-roasting department, Workshop offers fresh and enthusiastically researched brews in.</p>
+				<p class="item-desc">Ralf Rüller and his crew have installed themselves as the reference point for Third Wave Coffee in Berlin, despite only being open since end of 2010. But their distinct interior style and serious devotion to the art of coffee making has made them to internationally known ambassadors of coffee enthusiasts.</p>
 				<ul>
-					<a href="http://www.workshopcoffee.com/"><li><span class="link-icon">workshopcoffee.com</span></li></a>
-					<li><span class="location-icon">27 Clerkenwell Road</span></li>
+					<a href="http://barn.bigcartel.com/"><li><span class="link-icon">barn.bigcartel.com</span></li></a>
+					<li><span class="location-icon">Auguststraße 58</span></li>
 				</ul>
 				<div class="wide-image">
 					<img src="/_core/imgs/london/workshop.png"/>
@@ -94,10 +94,10 @@ include ("../_include/light-navigation.php");
 		<div class="loop-12">
 			<div class="loop-12">
 				<p class="item-name">Companion Coffee</p>
-				<p class="item-desc">Established in 2009 by owner James Dickson, Workshop Coffee specialises in the sourcing and roasting of speciality coffee. Supported by an experienced coffee-roasting department, Workshop offers fresh and enthusiastically researched brews in.</p>
+				<p class="item-desc">Shawn and Chris, the barristas behind Companion Coffee, came to Berlin to do coffee – starting out at The Barn in 2010, they went on to start their own bar a year ago. Located inside of Kreuzberg’s fashionable concept store Voo on Oranienstraße they are serving the best coffee in Kreuzberg 36 – see more in our review.</p>
 				<ul>
-					<a href="http://www.workshopcoffee.com/"><li><span class="link-icon">workshopcoffee.com</span></li></a>
-					<li><span class="location-icon">27 Clerkenwell Road</span></li>
+					<a href="https://www.facebook.com/pages/Companion-Coffee/317033645062941"><li><span class="link-icon">facebook.com/Companion-Coffee</span></li></a>
+					<li><span class="location-icon">Oranienstrasse 24</span></li>
 				</ul>
 				<div class="wide-image">
 					<img src="/_core/imgs/london/workshop.png"/>
